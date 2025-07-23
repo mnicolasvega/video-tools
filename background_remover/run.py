617a0b23c49f2589ef871efd62402b6e85c095b7
Nicolas Vega <vega.nicolas.m@gmail.com>
2025-07-23 11:24:35 -0300

    Remove converting image to RGB (already done in background_remover tool)

--- background_remover/run.py
+++ background_remover/run.py
@@ -15,5 +15,4 @@ def get_output_path(input: str, label: str) -> str:
 if __name__ == "__main__":
     image = background_remover.run(PATH_IMAGE_INPUT)
     output_path = get_output_path(PATH_IMAGE_INPUT, 'nobg')
-    image = image.convert('RGB')
     image.save(output_path)
