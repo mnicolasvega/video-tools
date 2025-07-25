4cf865ff09fd461c49c4b9c60b47ac91ef6c0c16
Nicolas Vega <vega.nicolas.m@gmail.com>
2025-07-25 00:06:45 -0300

    Add draw bounds

--- identifier/identifier.py
+++ identifier/identifier.py
@@ -1,15 +1,18 @@
+from numpy import ndarray
 from PIL import Image
 import cv2
 import torch
 
 CLASS_PERSON = 0
+REPOSITORY = 'ultralytics/yolov5'
+MODEL = 'yolov5s'
 
-def get_object_bounds(input_path: str) -> list:
+def get_object_bounds(input_path: str, _tag: str = 'person', _class: int = CLASS_PERSON) -> list:
     img = Image.open(input_path)
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+    model = torch.hub.load(REPOSITORY, MODEL, pretrained = True)
     results = model(img)
-    df_people = results.pandas().xyxy[CLASS_PERSON]
-    person_boxes = df_people[df_people['name'] == 'person']
+    df_people = results.pandas().xyxy[_class]
+    person_boxes = df_people[df_people['name'] == _tag]
     bounds = []
     for _, row in person_boxes.iterrows():
         confidence = row['confidence']
@@ -17,11 +20,16 @@ def get_object_bounds(input_path: str) -> list:
         bounds.append([x_min, y_min, x_max, y_max, confidence])
     return bounds
 
-def get_subimages(input_path: str, bounds: list) -> list:
-    img_source = cv2.imread(input_path)
+def get_subimages(img: ndarray, bounds: list) -> list:
     subimages = []
     for bound in bounds:
         x_min, y_min, x_max, y_max, confidence = bound
-        img_crop = img_source[y_min:y_max, x_min:x_max]
+        img_crop = img[y_min:y_max, x_min:x_max]
         subimages.append(img_crop)
     return subimages
+
+def draw_bounds(img: ndarray, bounds: list) -> None:
+    for bound in bounds:
+        x_min, y_min, x_max, y_max, confidence = bound
+        color = (128, 128, 255)
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color, 3)
--- identifier/run.py
+++ identifier/run.py
@@ -4,10 +4,8 @@ import cv2
 import identifier
 import os
 
-
 load_dotenv()
 PATH_IMAGE_INPUT = os.getenv('PATH_IMAGE_INPUT')
-
 
 def get_output_path(input: str, label: str) -> str:
     file_path = Path(input)
@@ -15,12 +13,16 @@ def get_output_path(input: str, label: str) -> str:
     extension = file_path.suffix
     return file_path.with_name(f"{file_name} {label}{extension}")
 
-
 if __name__ == "__main__":
     bounds = identifier.get_object_bounds(PATH_IMAGE_INPUT)
-    subimages = identifier.get_subimages(PATH_IMAGE_INPUT, bounds)
+    image = cv2.imread(PATH_IMAGE_INPUT)
+    subimages = identifier.get_subimages(image, bounds)
     i = 1
-    for image in subimages:
-        output_path = get_output_path(PATH_IMAGE_INPUT, f" person {i}")
-        cv2.imwrite(output_path, image)
+    for subimage in subimages:
+        subimage_path = get_output_path(PATH_IMAGE_INPUT, f" person {i}")
+        cv2.imwrite(subimage_path, subimage)
         i = i + 1
+
+    identifier.draw_bounds(image, bounds)
+    output_path = get_output_path(PATH_IMAGE_INPUT, f" bounds")
+    cv2.imwrite(output_path, image)
