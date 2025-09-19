30b7782736e78073f0af6f11a84521d29e31a29d
Nicolas Vega <vega.nicolas.m@gmail.com>
2025-09-19 20:31:15 -0300

    video_subtitle: Catch FileNotFound. Save .txt transcription

--- video_subtitle/video_subtitle.py
+++ video_subtitle/video_subtitle.py
@@ -12,7 +12,7 @@ MODEL = "large-v3"
 MODEL_LANGUAGE = "en"
 
 
-def transcribe(transcription: dict) -> str:
+def transcribe_srt(transcription: dict) -> str:
     subtitles = ""
     for i, segment in enumerate(transcription['segments']):
         start = segment['start']
@@ -24,6 +24,14 @@ def transcribe(transcription: dict) -> str:
     return subtitles
 
 
+def transcribe_txt(transcription: dict) -> str:
+    subtitles = ""
+    for i, segment in enumerate(transcription['segments']):
+        text = segment['text']
+        subtitles += f"{text}\n"
+    return subtitles
+
+
 def convert_to_time(segundo: int) -> str:
     hours = int(segundo // 3600)
     minutes = int((segundo % 3600) // 60)
@@ -32,23 +40,26 @@ def convert_to_time(segundo: int) -> str:
     return f"{hours:02}:{minutes:02}:{seconds:02},{milliseconds:03}"
 
 
-def run(model: whisper.Whisper, path_video: str, path_srt: str) -> None:
-    result = model.transcribe(
+def run(model: whisper.Whisper, path_video: str, path_srt: str, path_txt: str) -> None:
+    transcript = model.transcribe(
         path_video,
         task = "translate",
         language = MODEL_LANGUAGE
     )
     print(f"-> saving: {path_srt}")
-    srt_content = transcribe(result)
+    srt_content = transcribe_srt(transcript)
+    txt_content = transcribe_txt(transcript)
     with open(path_srt, "w", encoding="utf-8") as srt_file:
         srt_file.write(srt_content)
+    with open(path_txt, "w", encoding="utf-8") as txt_file:
+        txt_file.write(txt_content)
     print("-> saved")
 
 
-def run_timetracking(model: whisper.Whisper, path_video: str, path_srt: str) -> None:
+def run_timetracking(model: whisper.Whisper, path_video: str, path_srt: str, path_txt: str) -> None:
     print(f"-> transcribing: {path_video}")
     start_time = time.time()
-    run(model, path_video, path_srt)
+    run(model, path_video, path_srt, path_txt)
     end_time = time.time()
     seconds = end_time - start_time
     print(f" -> time elasped: %.2f seconds" % (seconds))
@@ -67,6 +78,8 @@ def get_model() -> whisper.Whisper:
 def get_files(path: str) -> list:
     try:
         return sorted(os.listdir(path))
+    except FileNotFoundError:
+        return []
     except PermissionError:
         return []
 
@@ -77,6 +90,7 @@ if __name__ == "__main__":
     model = get_model()
     for i, file_name in enumerate(files):
         path_input = f"{DIR_PATH}/{file_name}"
-        path_output = f"{DIR_PATH}/{file_name}.srt"
-        run_timetracking(model, path_input, path_output)
+        path_output_srt = f"{DIR_PATH}/{file_name}.srt"
+        path_output_txt = f"{DIR_PATH}/{file_name}.txt"
+        run_timetracking(model, path_input, path_output_srt, path_output_txt)
     print("-> end")
